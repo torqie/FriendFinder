96e6b0841644c27801d96bd7b6830bc42b42d440
Terik Hone <terikhone@gmail.com>
2020-05-20 22:42:25 -0600

    setup static files

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 // Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
-
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
-
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
 
 
-
+// Route Files
+// =============================================================
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
 
 
 // Starts the server to begin listening
